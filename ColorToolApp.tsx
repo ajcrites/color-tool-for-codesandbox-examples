9064a08e2e4fee0273e135ef5160ecfffab293fe
Andrew Crites <acrites@mobiquityinc.com>
2019-03-18 15:47:49 -0400

    e

--- ColorToolApp.tsx
+++ ColorToolApp.tsx
@@ -1,16 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, createContext } from 'react';
 
 import { HexInput } from '~/HexInput';
 import { RgbaInput } from '~/RgbaInput';
 
+interface ColorContextProps {
+  hex: string;
+  rgba: number[];
+
+  setHex: (value) => void;
+  setRgba: (value) => void;
+};
+
+const ColorContext = createContext<ColorContextProps>({} as any);
+
 export const ColorToolApp = () => {
   const [hex, setHex] = useState('');
   const [rgba, setRgba] = useState(['', '', '', '']);
+  const state: ColorContextProps = {
+    hex, rgba, setHex, setRgba,
+  };
 
   return (
     <main>
-      <HexInput hex={hex} setHex={setHex} />
-      <RgbaInput rgba={rgba} setRgba={setRgba} />
+      <ColorContext.Producer value={state}>
+        <HexInput />
+        <RgbaInput />
+      </ColorContext.Producer>
     </main>
   );
 };
--- HexInput.tsx
+++ HexInput.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 
-export const HexInput = ({ hex, setHex }) => {
+export const HexInput = ({ hex, setHex, setRgba }) => {
   const onChange = ({ target: { value } }) => {
     setHex(value);
+    setRgba(value);
   };
 
   return (
