d2c4d58afb6df67601bd2be83f19c6eaee28b0e9
Andrew Crites <acrites@mobiquityinc.com>
2019-03-18 17:56:45 -0400

    f

---
+++ ColorInput.tsx
@@ -0,0 +1,26 @@
+export const ColorInput = () => {
+  const { hex, setHex, setRgba } = useContext(ColorToolContext);
+  const input = useRef(null);
+
+  const [show, setShow] = useState(true);
+
+  useEffect(() => {
+    input.current.value = '!';
+    // input[type=color] is not supported
+    if ('!' === input.current.value) {
+      setShow(false);
+    }
+  });
+
+  const onChange = ({ target: { value } }) => {
+    setHex(value);
+    setRgba(value);
+  };
+
+  return show ? (
+    <label>
+      Color Picker:
+      <input type="color" ref={input} value={hex} onChange={onChange} />
+    </label>
+  ) : null;
+};
